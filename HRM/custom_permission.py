50f08d2cd32c99ad5534e703ca05b4c0faf239d9
Deepak-2 <deepak@zapbuild.com>
2021-10-01 14:21:38 +0530

    permission hr change

--- HRM/custom_permission.py
+++ HRM/custom_permission.py
@@ -4,7 +4,4 @@ from rest_framework.permissions import BasePermission
 class IsHREmployee(BasePermission):
 
     def has_permission(self, request, view):
-        print(request.user.is_employee)
-        print(request.user.id)
-        print(bool(request.user) and (request.user.is_employee==False))
-        return bool(request.user) and (request.user.is_employee==False)
+        return bool(request.user) and (request.user.is_hr==True)
